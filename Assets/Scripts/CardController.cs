5b96af6736d4cfcec45e2a3714209ad3413cc3df
Aleksey-Trifonov <altrishka@gmail.com>
2022-09-02 12:24:37 -0400

    Preliminary card prefab

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField] private Image cardGlow;
+    [SerializeField] private Image cardImage;
+    [SerializeField] private TextMeshProUGUI attackValue;
+    [SerializeField] private TextMeshProUGUI healthValue;
+    [SerializeField] private TextMeshProUGUI manaValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
